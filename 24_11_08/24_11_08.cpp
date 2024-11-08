8e286e3c03bce46a73647a757d3d503b7deeb089
Scher_xb69 <Scher_xb69@8-6>
2024-11-08 17:39:29 +0200

    Add ChangeInfoAboutStudent in Student class

--- 24_11_08/24_11_08.cpp
+++ 24_11_08/24_11_08.cpp
@@ -14,14 +14,23 @@ public:
 	}
 	Student(const Student& obj) {
 		this->name = obj.name;
+
 		this->age = obj.age;
 	}
 	void PrintInfoAboutStudent() {
 		cout << "Student's name: " << this->name << endl;
 		cout << "Student's age: " << this->age << endl;
 	}
+	void ChangeInfoAboutStudent() {
+		cout << "Enter a new name for student: ";
+		cin >> this->name;
+		cout << "Enter a new age for student: ";
+		cin >> this->age;
+	}
 };
 int main() {
 	Student obj;
+	obj.PrintInfoAboutStudent();
+	obj.ChangeInfoAboutStudent();
 	obj.PrintInfoAboutStudent();
 }
